425bb21a6433d4ef9ff6a48b0c9d4c35955ca7a8
Chris Holdgraf <choldgraf@berkeley.edu>
2018-06-11 09:21:20 -0700

    flit

--- makeitpop/__init__.py
+++ makeitpop/__init__.py
@@ -1,4 +1,5 @@
+"""Warp your data using the same perceptual warping that Jet applies!"""
 from .derivatives import update_derivatives
 from .makeitpop import makeitpop, cmaps
 
-__version__ = 0.1
+__version__ = "0.1"
